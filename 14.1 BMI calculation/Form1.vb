930be504226e97cf48015cd1df072a7a40f35ed5
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-06 08:01:08 +0200

    Add project files.

---
+++ 14.1 BMI calculation/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Drawing.Text
+
+Public Class Form1
+    Private Function BMI(Height As Single, Weight As Single) As Double
+        BMI = Weight / Height ^ 2
+
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim H As Single, W As Single
+        H = Val(TextBox1.Text)
+        W = Val(TextBox2.Text)
+
+        TextBox3.Text = Format(BMI(H, W), "0.00")
+
+
+
+
+
+    End Sub
+
+
+End Class
